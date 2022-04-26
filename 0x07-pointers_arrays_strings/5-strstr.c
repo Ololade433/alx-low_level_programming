1d7112267b4ab60f177717227e5a60440d9cdbd6
Ololade433 <bokunsanya@gmail.com>
2022-04-26 10:03:56 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _strstr - locates a substring
+* @needle: first substring occurence
+* @haystack: string to find substring in
+* Return: pointer to first char of matching substring
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int k;
+
+	while (*haystack != '\0')
+	{
+		k = 0;
+		while (*haystack == *needle && *haystack != '\0' && *needle != '\0')
+			haystack++, needle++, k++;
+		if (*needle == '\0')
+			return (haystack - k);
+		haystack -= (k - 1), needle -= k;
+	}
+	return ('\0');
+}
